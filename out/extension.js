395a0ed1a2aa61319c017cfa8d858b88259c2d30
Abheek Tripathy <abheek.tripathy@gmail.com>
2023-04-06 23:23:54 +0530

    fixing up stuff

--- out/extension.js
+++ out/extension.js
@@ -1,45 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
-/* eslint-disable @typescript-eslint/naming-convention */
 const vscode = require("vscode");
-const openai_1 = require("openai");
-const dotenv = require("dotenv");
-dotenv.config();
 function activate(context) {
-    console.log('Congratulations, your extension "devsh" is now active!');
-    let disposable = vscode.commands.registerCommand('extension.explainCodeSnippet', async () => {
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const selectedText = editor.document.getText(editor.selection);
-            await explainCode(selectedText);
-        }
-        else {
-            vscode.window.showInformationMessage('No text selected');
-        }
+    // Create a new webview panel
+    const panel = vscode.window.createWebviewPanel('webviewPanel', 'My Webview Panel', vscode.ViewColumn.Beside, {
+        enableScripts: true,
+        retainContextWhenHidden: true,
     });
-    context.subscriptions.push(disposable);
+    // Set the HTML content for the panel
+    panel.webview.html = '<h1>Hello, World!</h1>';
+    // Add the panel to the sidebar
+    context.subscriptions.push(panel);
 }
 exports.activate = activate;
-async function explainCode(code) {
-    const apikey = "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";
-    if (!apikey) {
-        throw new Error('OpenAI API key not found');
-    }
-    const config = new openai_1.Configuration({
-        apiKey: apikey
-    });
-    const openai = new openai_1.OpenAIApi(config);
-    const prompt = `explain the ${code} in plain text english`;
-    const response = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `${prompt}`,
-        temperature: 0.4,
-        max_tokens: 250,
-    });
-    const output = response.data.choices[0].text;
-    vscode.window.showInformationMessage(`${output}`);
-}
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,22 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import * as vscode from 'vscode';
-import * as openai from 'openai';
-import { Configuration, OpenAIApi } from 'openai';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 export function activate(context: vscode.ExtensionContext) {
+	// Create a new webview panel
+	const panel = vscode.window.createWebviewPanel(
+	  'webviewPanel',
+	  'My Webview Panel',
+	  vscode.ViewColumn.Beside,
+	  {
+		enableScripts: true,
+		retainContextWhenHidden: true,
+	  }
+	);
+  
+	// Set the HTML content for the panel
+	panel.webview.html = '<h1>Hello, World!</h1>';
+  
+	// Add the panel to the sidebar
+	context.subscriptions.push(panel);
+  }
 
-	console.log('Congratulations, your extension "devsh" is now active!');
-	let disposable = vscode.commands.registerCommand('extension.explainCodeSnippet', async() => {
-
-
-	const editor = vscode.window.activeTextEditor;
-	if(editor) {
-		const selectedText= editor.document.getText(editor.selection);
-		await explainCode(selectedText);
-	}
-	else{
-		vscode.window.showInformationMessage('No text selected');
-	}
-	});
-
-	context.subscriptions.push(disposable);
-}
-
-async function explainCode(code: string){
-	const apikey = process.env.OPENAI_API_KEY;
-	if(!apikey){
-		throw new Error('OpenAI API key not found');
-	}
-	
-		const config = new Configuration({
-			apiKey: apikey
-		});
-
-	const openai = new OpenAIApi(config);
-	const prompt = `explain the ${code} in plain text english`;
-	const response = await openai.createCompletion({
-		model: 'text-davinci-003',
-		prompt: `${prompt}`,
-		temperature: 0.4,
-		max_tokens: 250,
-	});
-	
-	const output = response.data.choices[0].text;
-	vscode.window.showInformationMessage(`${output}`); 
-
-}
 export function deactivate() {}
