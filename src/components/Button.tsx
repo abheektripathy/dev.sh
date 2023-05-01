2aaa273f1203c409ee1c37fc5e92e6eed8013f89
Abheek Tripathy <abheek.tripathy@gmail.com>
2023-05-02 03:28:13 +0530

    ok

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import { EXTENSION_CONSTANT } from 'constant';
 function Button(){
     return (
         <button className='button' id={EXTENSION_CONSTANT.ELEMENT_IDS.TRIGGER_MESSAGE_BUTTON}>
-            explain up
+            explain
         </button>
     );
 }
